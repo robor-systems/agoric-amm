65e48399fe5ebdb07d185afa0997e20311b0309d
danish <danishyasin33@gmail.com>
2021-10-06 15:53:59 +0500

    move asset function to helpers

--- ui/src/components/Swap/DialogSwap/AssetDialog.jsx
+++ ui/src/components/Swap/DialogSwap/AssetDialog.jsx
@@ -3,13 +3,8 @@ import { useApplicationContext } from 'context/Application';
 import AssetContext from 'context/AssetContext';
 import React, { useContext, useEffect, useState } from 'react';
 import _ from 'lodash';
-import { v4 as uuidv4 } from 'uuid';
 
-import agoricLogo from 'assets/crypto-icons/agoric-logo.png';
-import atomLogo from 'assets/crypto-icons/atom-logo.png';
-import bldLogo from 'assets/crypto-icons/bld-logo.png';
-import kirkLogo from 'assets/crypto-icons/kirk-logo.png';
-import usdcLogo from 'assets/crypto-icons/usdc-logo.png';
+import { getAssets } from 'utils/helpers';
 
 import AssetListItem from '../ListItem/AssetListItem';
 import ListItem from '../ListItem/ListItem';
@@ -25,76 +20,11 @@ const AssetDialog = ({ type }) => {
   const { purses } = state;
 
   useEffect(() => {
-    const filteredAssets = purses?.filter(
+    const filteredPurses = purses?.filter(
       purse => purse.displayInfo.assetKind !== 'set',
     );
 
-    // used for storing intermediate response
-    const interArr = [];
-    filteredAssets?.forEach(purse => {
-      // balances noted in bigInt, converting them using the provided decimalplaces
-      const balance =
-        Number(purse.currentAmount?.value) /
-        10 ** purse.displayInfo?.decimalPlaces;
-
-      // if such asset already inserted
-      const similarAssetIndex = interArr.findIndex(elem => {
-        return elem.code === purse.brandPetname;
-      });
-
-      if (similarAssetIndex !== -1) {
-        interArr[similarAssetIndex].balance += balance;
-
-        interArr[similarAssetIndex].purses.push({
-          id: uuidv4(),
-          name: purse.pursePetname,
-          balance,
-          // cmt(danish): balance USD not available right now
-          balanceUSD: undefined,
-        });
-        // skip if already includes but add in purse
-        console.log('skipping: ', purse);
-        return;
-      }
-
-      // setting default image as agoric logo
-      let image = agoricLogo;
-      switch (purse.brandPetname) {
-        case 'RUN':
-          image = agoricLogo;
-          break;
-        case 'BLD':
-          image = bldLogo;
-          break;
-        case 'LINK':
-          image = kirkLogo;
-          break;
-        case 'USDC':
-          image = usdcLogo;
-          break;
-        default:
-          break;
-      }
-
-      interArr.push({
-        id: uuidv4(),
-        code: purse.brandPetname,
-        name: purse.brandPetname,
-        // cmt(danish): no images defined now
-        image,
-        balance,
-        purses: [
-          {
-            id: uuidv4(),
-            name: purse.pursePetname,
-            balance,
-            // cmt(danish): balance USD not available right now
-            balanceUSD: undefined,
-          },
-        ],
-      });
-    });
-    setAssets([...interArr]);
+    setAssets([...getAssets(filteredPurses)]);
   }, [purses]);
 
   useEffect(() => {
---
+++ ui/src/utils/helpers.js
@@ -0,0 +1,76 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import agoricLogo from 'assets/crypto-icons/agoric-logo.png';
+import bldLogo from 'assets/crypto-icons/bld-logo.png';
+import kirkLogo from 'assets/crypto-icons/kirk-logo.png';
+import usdcLogo from 'assets/crypto-icons/usdc-logo.png';
+
+export const getAssets = filteredPurses => {
+  // used for storing intermediate response
+  const interArr = [];
+  filteredPurses?.forEach(purse => {
+    // balances noted in bigInt, converting them using the provided decimalplaces
+    const balance =
+      Number(purse.currentAmount?.value) /
+      10 ** purse.displayInfo?.decimalPlaces;
+
+    // if such asset already inserted
+    const similarAssetIndex = interArr.findIndex(elem => {
+      return elem.code === purse.brandPetname;
+    });
+
+    if (similarAssetIndex !== -1) {
+      interArr[similarAssetIndex].balance += balance;
+
+      interArr[similarAssetIndex].purses.push({
+        id: uuidv4(),
+        name: purse.pursePetname,
+        balance,
+        // cmt(danish): balance USD not available right now
+        balanceUSD: undefined,
+      });
+      // skip if already includes but add in purse
+      console.log('skipping: ', purse);
+      return;
+    }
+
+    // setting default image as agoric logo
+    let image = agoricLogo;
+    switch (purse.brandPetname) {
+      case 'RUN':
+        image = agoricLogo;
+        break;
+      case 'BLD':
+        image = bldLogo;
+        break;
+      case 'LINK':
+        image = kirkLogo;
+        break;
+      case 'USDC':
+        image = usdcLogo;
+        break;
+      default:
+        break;
+    }
+
+    interArr.push({
+      id: uuidv4(),
+      code: purse.brandPetname,
+      name: purse.brandPetname,
+      // cmt(danish): no images defined now
+      image,
+      balance,
+      purses: [
+        {
+          id: uuidv4(),
+          name: purse.pursePetname,
+          balance,
+          // cmt(danish): balance USD not available right now
+          balanceUSD: undefined,
+        },
+      ],
+    });
+  });
+
+  return interArr;
+};
