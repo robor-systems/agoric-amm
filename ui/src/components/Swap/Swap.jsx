20108d54201557dd826f338a5ac26c5bc8cf520c
danish <danishyasin33@gmail.com>
2021-10-06 19:45:27 +0500

    move comment

--- ui/src/components/Swap/Swap.jsx
+++ ui/src/components/Swap/Swap.jsx
@@ -63,9 +63,25 @@ const Swap = () => {
     });
   };
 
+  /**
+   * The `marketRate` is the ratio between the input asset
+   * and the output asset. It is computed by getting the market
+   * price for each pool, and composing them. If one of the
+   * selected assets is the central token, that "poolRate"
+   * is just 1:1 (centralOnlyRate, above).
+   *
+   * Becuase the ratios are queries async, the state for
+   * them starts as `{ brand, amount: null }`. The brand is
+   * used to check at `set` time that the brand has not changed;
+   * e.g., because the user selected a purse with a different
+   * brand.
+   *
+   * The input `poolRate` is `RUN/inputBrand` and the output
+   * `poolRate` is `outputBrand/RUN`.
+   */
+
   useEffect(() => {
     const getRates = async () => {
-      console.log(asset);
       let inputRate = null;
       asset.from &&
         (inputRate = await requestRatio(
--- ui/src/services/swap.service.js
+++ ui/src/services/swap.service.js
@@ -1,23 +1,6 @@
 import { E } from '@agoric/captp';
 import { makeRatio } from '@agoric/zoe/src/contractSupport';
 
-/**
- * The `marketRate` is the ratio between the input asset
- * and the output asset. It is computed by getting the market
- * price for each pool, and composing them. If one of the
- * selected assets is the central token, that "poolRate"
- * is just 1:1 (centralOnlyRate, above).
- *
- * Becuase the ratios are queries async, the state for
- * them starts as `{ brand, amount: null }`. The brand is
- * used to check at `set` time that the brand has not changed;
- * e.g., because the user selected a purse with a different
- * brand.
- *
- * The input `poolRate` is `RUN/inputBrand` and the output
- * `poolRate` is `outputBrand/RUN`.
- */
-
 export const requestRatio = async (brand, makeRate, centralBrand, ammAPI) => {
   if (brand === centralBrand) {
     console.log('got central asset');
